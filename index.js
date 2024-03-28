488c1c9f5589252bf420ef87350156050fd4eaf5
Rich Cusimano <rcusimano@ingenio.com>
2024-03-28 14:31:47 -0700

    fixed display of var

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const appEnv = process.env.APP_ENV || 'No env specified';
 const server = http.createServer((req, res) => {
  console.log(req.headers);
  res.statusCode = 200;
- res.end("<html><body><h1>Hello, World ${appEnv}</h1></body></html>");
+ res.end(`<html><body><h1>Hello, World ${appEnv}</h1></body></html>`);
 })
 server.listen(port, hostname);
